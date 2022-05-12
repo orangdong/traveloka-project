bffabd48cef3c53a730507291b5a5f5ddf309430
Hans Evan Tatipata <thansevan@gmail.com>
2022-05-12 13:11:04 +0700

    mengerjakan sisa endpoints dari 5 yang telah diberikan

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,14 +1,14 @@
 import express from 'express';
-import { index, show, update, destroy, store } from '../handlers/hotels.js';
+import { index, show, update, destroy, store, indexReviews } from '../handlers/hotels.js';
 
 const router = express.Router();
 
 router.get('/', index);
 router.post('/', store);
+router.get('/reviews', indexReviews);
 router.get('/:id', show);
 router.put('/:id', update);
 router.delete('/:id', destroy);
-router.get('/reviews', index);
 router.get('/:id/reviews', index);
 
 export default router;
