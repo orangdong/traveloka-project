5a4f5e528528c30a7d5478c114afbc113b74fa90
Aviel Naftali <68643310+orangdong@users.noreply.github.com>
2022-05-12 19:48:27 +0700

    delete unused routes

--- handlers/hotels.js
+++ handlers/hotels.js
@@ -14,13 +14,6 @@ const index = async (req, res) => {
     });
 }
 
-const store = async (req, res) => {
-    return res.json({
-        status: 'success',
-        message: 'Post routes'
-    })
-}
-
 const show = async (req, res) => {
     const id = req.params.id;
 
@@ -41,22 +34,6 @@ const show = async (req, res) => {
         status: 'success',
         message: `Show Route id: ${id}`,
         data: hotel
-    });
-}
-
-const update = async (req, res) => {
-    const id = req.params.id;
-
-    if(!id || !Number.isInteger(parseInt(id))) { 
-        return res.status(400).json({
-            status: 'error',
-            message: 'Invalid id parameter'
-        })
-    }
-
-    return res.json({
-        status: 'success',
-        message: `Update Route id: ${id}`
     });
 }
 
--- routes/hotels.js
+++ routes/hotels.js
@@ -4,11 +4,7 @@ import { index, show, update, destroy, store, indexReviews } from '../handlers/h
 const router = express.Router();
 
 router.get('/', index);
-router.post('/', store);
 router.get('/reviews', indexReviews);
 router.get('/:id', show);
-router.put('/:id', update);
-router.delete('/:id', destroy);
-router.get('/:id/reviews', index);
 
 export default router;
