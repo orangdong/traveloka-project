2d3a2879417090cd4402e36f48f3c34c9f0643b7
Malik Wali <defdotpy@gmail.com>
2022-05-14 15:47:47 +0700

    feat: store and update reviews

--- handlers/reviews.js
+++ handlers/reviews.js
@@ -1,26 +1,3 @@
-
-// const store = async (req, res) => {
-//     return res.json({
-//         status: 'success',
-//         message: 'Post routes'
-//     })
-// }
-
-// const show = async (req, res) => {
-//     const id = req.params.id;
-
-//     if(!id || !Number.isInteger(parseInt(id))) { 
-//         return res.status(400).json({
-//             status: 'error',
-//             message: 'Invalid id parameter'
-//         })
-//     }
-
-//     return res.json({
-//         status: 'success',
-//         message: `Show Route id: ${id}`
-//     });
-// }
 import pkg from '@prisma/client';
 const { PrismaClient } = pkg;
 
@@ -67,20 +44,74 @@ const show = async (req, res) => {
 }
 
 const store = async (req, res) => {
+    const {user_id, hotel_id, rating, comment} = req.body;
+
+    if(!user_id || !hotel_id || !rating || comment) {
+        return res.status(400).json({
+            status: 'error',
+            message: 'Missing required fields'
+        })
+    }
+
+    const review = await prisma.review.create({
+        data: {
+            user_id,
+            hotel_id,
+            rating,
+            comment
+        }
+    })
+
+
     return res.json({
         status: 'success',
-        message: 'Post routes'
+        message: 'Review created',
+        data: review
     })
 }
 
 const update = async (req, res) => {
     const id = req.params.id;
+
     if(!id || !Number.isInteger(parseInt(id))) { 
         return res.status(400).json({
             status: 'error',
             message: 'Invalid id parameter'
         })
     }
+    
+    const review = await prisma.review.findUnique({
+        where: {
+          id: parseInt(id),
+        },
+    })
+
+    if(!review) { 
+        return res.status(404).json({
+            status: 'error',
+            message: 'Review not found'
+        })
+    }
+    const {rating, comment} = req.body;
+
+    if(!rating || !comment ) {
+        return res.status(400).json({
+            status: 'error',
+            message: 'Missing required fields'
+        })
+    }
+
+    const updatedReview = await prisma.user.update({
+        where: {
+            id: parseInt(id),
+        },
+        data: {
+            rating: rating,
+            comment: comment,
+            data: updatedReview,
+        }
+    });
+
     return res.json({
         status: 'success',
         message: `Update Route id: ${id}`
@@ -95,9 +126,29 @@ const destroy = async (req, res) => {
             message: 'Invalid id parameter'
         })
     }
+
+    const review = await prisma.review.findUnique({
+        where: {
+          id: parseInt(id),
+        },
+    })
+
+    if(!review) { 
+        return res.status(404).json({
+            status: 'error',
+            message: 'Review not found'
+        })
+    }
+
+    await prisma.user.delete({
+        where: { id: parseInt(id) },
+    })
+
     return res.json({
         status: 'success',
         message: `Delete Route id: ${id}`
     });
 }
+
+
 export { index, show, store, update, destroy };
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { index, show } from '../handlers/reviews.js';
+import { index, show, destroy, update, store } from '../handlers/reviews.js';
 
 const router = express.Router();
 
 router.get('/', index);
 router.get('/:id', show);
-// router.post('/', store);
-// router.delete('/:id', destroy);
+router.post('/', store);
+router.delete('/:id', destroy);
+router.put('/:id', update);
 
 export default router;
