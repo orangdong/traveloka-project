65464147790af5f44b8ff406bbdde62cc4b967ec
Aviel Naftali <dev.naftaliaviel@gmail.com>
2022-06-13 11:24:13 +0700

    update schema

--- prisma/migrations/20220613042215_init/migration.sql
+++ prisma/migrations/20220613042215_init/migration.sql
@@ -3,12 +3,10 @@ CREATE TABLE `users` (
     `id` VARCHAR(191) NOT NULL,
     `firebaseId` VARCHAR(191) NULL,
     `name` VARCHAR(191) NOT NULL,
-    `username` VARCHAR(191) NOT NULL,
     `city` VARCHAR(191) NOT NULL,
     `state` VARCHAR(191) NOT NULL,
 
     UNIQUE INDEX `users_firebaseId_key`(`firebaseId`),
-    UNIQUE INDEX `users_username_key`(`username`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
